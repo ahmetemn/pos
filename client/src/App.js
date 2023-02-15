764176e47a6b151f9322c2957bf29490ff11751c
antares <antares@cipherbiz.com>
2023-02-15 19:48:58 +0300

    add tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,18 @@ import { Button, Space } from 'antd';
 import "./App.css"
 function App() {
   return (
-        <>
-          
-        </>
+
+    <div className='button'>
+
+      <Button className='btn' type='primary' >Primary Button</Button>
+
+
+
+      <h1 className="text-3xl font-bold underline text-red-600 ">
+        Hello world!
+      </h1>
+    </div>
+
   );
 }
 
