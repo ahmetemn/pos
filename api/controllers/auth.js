153a606b1d0a915b0167da19b29fba372fa0e411
antares <antares@cipherbiz.com>
2023-02-24 19:30:05 +0300

    hash password

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,15 +1,22 @@
 
 
 import User from "../models/User.js";
-
+import bcryptjs from "bcryptjs";
 
 export const  registerUser = async (req,res , next ) =>{
+    const salt =  await bcryptjs.genSaltSync(8);
+    const hash = await bcryptjs.hashSync(req.body.password, salt);
 
-        const newUser = new User(req.body);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+            isAdmin:req.body.isAdmin
+        });
     try {
         
-        const savedUser = await newUser.save();
-        res.status(200).json(savedUser)
+       await newUser.save();
+        res.status(200).json("User has been created")
 
 
     } catch (error) {
