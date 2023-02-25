952571b41e0d444d0bb00d6799debca1eb7ba289
antares <antares@cipherbiz.com>
2023-02-25 11:39:13 +0300

    post category  , get category

--- client/src/components/categories/Categories.js
+++ client/src/components/categories/Categories.js
@@ -1,20 +1,68 @@
 
+import "./Style.css";
+import {Form, Modal  , Input, Button, message} from 'antd';
+import { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons"
+const Categories = ({categories}) => {
+    const [form ] = Form.useForm()
 
-   const Categories = () =>{
+    const onFinish = (values) =>{
+
+        try {
+            
+            fetch("http://localhost:8800/api/category", {
+                method:"POST",
+                body:JSON.stringify(values),
+                headers:{"Content-type":"application/json; charset=UTF-8"},
+            })
+            message.success("Category added")
+            form.resetFields()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
-    
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const showModal = () => {
+        setIsAddModalOpen(true);
+      };
+      const handleCancel = () => {
+        setIsAddModalOpen(false);
+      };
     return (
         <>
 
 
-        <ul className=" gap-5 flex md:flex-col text-lg">
-            <li className="bg-green-700 px-8 py-8 text-white cursor-pointer hover:bg-pink-700 transition-all font-bold text-center">All</li>
-            <li className="bg-green-700 px-8 py-8 text-white cursor-pointer hover:bg-pink-700 transition-all font-bold  text-center">Food</li>
-            <li className="bg-green-700 px-8 py-8 text-white cursor-pointer hover:bg-pink-700 transition-all font-bold  text-center">Drinks</li>
-            <li className="bg-green-700 px-8 py-8 text-white cursor-pointer hover:bg-pink-700 transition-all font-bold  text-center">All</li>
-        
-        </ul>
+            <ul className=" gap-5 flex md:flex-col text-lg">
+
+               {
+                 categories.map((item , i ) =>{
+                    return(
+                        <div key={i}>
+
+                        <li className="categroy-item">{item.title}</li>
+                        </div>
+                       
+                    )
+                 })
+               }
+                
+              
+
+
+                <li   onClick={showModal} className="categroy-item !bg-purple-800 hover:opacity-80" >
+                    <PlusOutlined />
+                </li>
+                <Modal title="Add Category"  open={isAddModalOpen}   onCancel={handleCancel} footer={false}>
+                     <Form  className="font-bold"  layout="vertical"  onFinish={onFinish} form ={form}>
+                         <Form.Item name="title" label="Category"    rules={[{ required: true  , message:"Category is required !"}]} >
+                             <Input />
+                         </Form.Item>
+                         <Button block type="primary" htmlType="submit" className="font-bold ">Create</Button>
+                     </Form>
+                </Modal>
+            </ul>
         </>
     )
 }
