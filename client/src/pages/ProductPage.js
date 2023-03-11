5835bd691f33ee53ed7bc1cf8cc4389c845ef060
antares <antares@cipherbiz.com>
2023-03-11 11:51:06 +0300

    add

---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,15 @@
+
+  
+
+   import React from 'react'
+   import Edit from "../components/products/Edit";
+   const ProductPage = () => {
+     return (
+       <div className='px-6'>
+        <h1 className='text-4xl font-bold text-center '>Products</h1>
+        <Edit /> 
+       </div>
+     )
+   }
+   
+   export default ProductPage;
---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+  
+import {configureStore} from "@reduxjs/toolkit";
+import cardSlice from "./cardSlice";
+
+
+   export default configureStore({
+         
+        reducer:{
+                card:cardSlice
+        }
+    });
+
